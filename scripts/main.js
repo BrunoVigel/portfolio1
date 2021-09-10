a3e9420f10602cb8609505dfb22a88986086b16d
Bruno Vigel <brunorosav@gmail.com>
2021-09-10 16:42:58 -0300

    isolamento de funcoes

--- scripts/main.js
+++ scripts/main.js
@@ -18,62 +18,70 @@ var swiper = new Swiper('.mySwiper', {
   }
 })
 
-//exit intent
-const exit = e => {
-  const shouldExit =
-    [...e.target.classList].includes('exit-intent-popup') || // user clicks on mask
-    e.target.className === 'close' || // user clicks on the close icon
-    e.keyCode === 27 // user hits escape
+function initExitIntent() {
+  //exit intent
+  const exit = e => {
+    const shouldExit =
+      [...e.target.classList].includes('exit-intent-popup') || // user clicks on mask
+      e.target.className === 'close' || // user clicks on the close icon
+      e.keyCode === 27 // user hits escape
 
-  if (shouldExit) {
-    document.querySelector('.exit-intent-popup').classList.remove('visible')
+    if (shouldExit) {
+      document.querySelector('.exit-intent-popup').classList.remove('visible')
+    }
+  }
+
+  const mouseEvent = e => {
+    const shouldShowExitIntent =
+      !e.toElement && !e.relatedTarget && e.clientY < 10
+
+    if (shouldShowExitIntent) {
+      document.removeEventListener('mouseout', mouseEvent)
+      document.querySelector('.exit-intent-popup').classList.add('visible')
+
+      CookieService.setCookie('exitIntentShown', true, 30)
+    }
+  }
+
+  if (!CookieService.getCookie('exitIntentShown')) {
+    setTimeout(() => {
+      document.addEventListener('mouseout', mouseEvent)
+      document.addEventListener('keydown', exit)
+      document.querySelector('.exit-intent-popup').addEventListener('click', exit)
+    }, 0)
   }
 }
+initExitIntent()
 
-const mouseEvent = e => {
-  const shouldShowExitIntent =
-    !e.toElement && !e.relatedTarget && e.clientY < 10
+function initMenu() {
+  // Abre e fecha o menu quando clicar no icone: hamburguer e x
+  const nav = document.querySelector('nav')
+  const toggle = document.querySelectorAll('nav .toggle')
 
-  if (shouldShowExitIntent) {
-    document.removeEventListener('mouseout', mouseEvent)
-    document.querySelector('.exit-intent-popup').classList.add('visible')
-
-    CookieService.setCookie('exitIntentShown', true, 30)
+  for (const element of toggle) {
+    element.addEventListener('click', function () {
+      nav.classList.toggle('show')
+    })
   }
 }
+initMenu()
 
-if (!CookieService.getCookie('exitIntentShown')) {
-  setTimeout(() => {
-    document.addEventListener('mouseout', mouseEvent)
-    document.addEventListener('keydown', exit)
-    document.querySelector('.exit-intent-popup').addEventListener('click', exit)
-  }, 0)
+function initHideMenu() {
+  /* quando clicar em um item do menu, esconder o menu */
+  const links = document.querySelectorAll('nav ul li a')
+
+  for (const link of links) {
+    link.addEventListener('click', function () {
+      nav.classList.remove('show')
+    })
+  }
 }
-
-// Abre e fecha o menu quando clicar no icone: hamburguer e x
-const nav = document.querySelector('nav')
-const toggle = document.querySelectorAll('nav .toggle')
-
-for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
-}
-
-/* quando clicar em um item do menu, esconder o menu */
-const links = document.querySelectorAll('nav ul li a')
-
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
-}
+initHideMenu()
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('header')
-const navHeight = header.offsetHeight
-
 function changeHeaderWhenScroll() {
+  const header = document.querySelector('header')
+  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -83,9 +91,10 @@ function changeHeaderWhenScroll() {
   }
 }
 
-const backToTopButton = document.querySelector('.back-to-top')
+  
 
 function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
   if (window.scrollY >= 300) {
     backToTopButton.classList.add('show')
   } else {
