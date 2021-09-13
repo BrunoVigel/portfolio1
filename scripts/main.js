3a769b69883f495514f6468433224346c17e8252
Bruno Vigel <brunorosav@gmail.com>
2021-09-13 19:52:07 -0300

    correcao bug menu

--- scripts/main.js
+++ scripts/main.js
@@ -68,6 +68,7 @@ initMenu()
 
 function initHideMenu() {
   /* quando clicar em um item do menu, esconder o menu */
+  const nav = document.querySelector('nav')
   const links = document.querySelectorAll('nav ul li a')
 
   for (const link of links) {
